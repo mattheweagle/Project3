8d4a2b003f52b61db4ef83ce861add0845fb5ca4
mattheweagle <eagle_matthew@msn.com>
2018-10-20 13:07:13 -0500

    added shooting

---
+++ Portal/Assets/Scripts/PortalMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalMovement : MonoBehaviour {
+    public Vector2 target;
+    Rigidbody2D rb;
+    public float speed = 30f;
+    Vector2 moveDirection;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        moveDirection.x = target.x - transform.position.x;
+        moveDirection.y = target.y - transform.position.y;
+        moveDirection = (moveDirection).normalized * speed;
+        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
