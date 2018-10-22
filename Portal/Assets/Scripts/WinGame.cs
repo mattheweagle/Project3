1cad532fb52722998e393094f68c59912a2d78fc
natleygam <42809084+natleygam@users.noreply.github.com>
2018-10-21 22:42:58 -0500

    Update WinGame.cs

--- Portal/Assets/Scripts/WinGame.cs
+++ Portal/Assets/Scripts/WinGame.cs
@@ -6,7 +6,8 @@ public class WinGame : MonoBehaviour {
 
     public GameObject youWin;
   
-    /** pre: Player reaches the door object
+    /** OnCollisionEnter2D(Collision2D collision)
+     *  pre: Player reaches the door object ("collides")
      *  post: Success message is displayed indicating level completed
      *  After Player "collides" with door object it will display a Win message indicating level completion
      */
